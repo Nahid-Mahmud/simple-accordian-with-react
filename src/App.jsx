297033d104aad1c4735485aea91cb60912b07df5
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-05-06 13:38:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Accordian from "./components/Accordian";
+
+function App() {
+  return (
+    <div className="bg-white h-screen w-screen  ">
+      {/* accordian */}
+      <div className="">
+        <Accordian />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/components/Accordian.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Accordian = () => {
+  const [selected, setSelected] = useState("");
+
+  const handleSingleSecection = (id) => {
+    setSelected(id === selected ? "" : id);
+  };
+  return (
+    <div className="flex h-screen justify-center items-center px-2">
+      <div className="w-full max-w-lg flex flex-col gap-2 ">
+        {accordianData?.map((item) => (
+          <div key={item?.id} onClick={() => handleSingleSecection(item?.id)}>
+            <div className="bg-[#153448] p-5 max-w-lg text-white rounded-md">
+              <div className="flex justify-between min-w-full text-xl">
+                {item?.title} <span> {selected === item?.id ? "-" : "+"} </span>
+              </div>
+              {selected === item?.id && <div className="mt-2">{item?.content}</div>}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
+
+const accordianData = [
+  {
+    id: crypto.randomUUID(),
+    title: "Introduction to React",
+    content:
+      "Learn the basics of ReactJS, including what it is, how it works, and why it's used. Topics include virtual DOM, components, JSX syntax, and more.",
+  },
+
+  {
+    id: crypto.randomUUID(),
+    title: "Creating Components",
+    content:
+      "Understand how to create React components, including functional components and class components. Explore component lifecycle methods, props, and state management.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "State Management with Redux",
+    content:
+      "Dive into state management in React using Redux. Learn how to set up Redux in a React application, create actions, reducers, and connect components to the Redux store.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Routing with React Router",
+    content:
+      "Explore client-side routing in React using React Router. Learn how to set up routes, handle navigation, and pass parameters to components.",
+  },
+];
